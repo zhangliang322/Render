135262e3d82dbdb5cc8cc718ffb91f61be3f853f
zhangliang322 <lianzhang8099@163.com>
2022-09-03 13:54:28 +0800

    line2,无对称性修复和高低校正

--- main.cpp
+++ main.cpp
@@ -3,17 +3,21 @@ const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
 //draw a line ,逐个顶点 0.01连成线
 void line(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color) {
-    for (float t = 0.; t < 1.; t += .01) {
-        int x = x0 + (x1 - x0) * t;
-        int y = y0 + (y1 - y0) * t;
+    for (int x=x0; x <=x1; x++)
+    {
+        float t = (x - x0) / (float)(x1 - x0);//浮点数除法
+        int y = y0 * (1. - t) + y1 * t;
         image.set(x, y, color);
+
     }
 }
 int main(int argc, char** argv) {
     //文件绘制
     TGAImage image(100, 100, TGAImage::RGB);
     line(13, 20, 80, 40, image, white);
+    line(20, 13, 40, 80, image, red);
+    line(80, 40, 13, 20, image, red);//红线缺失，和line1是重复的一条线，说明有对称性问题
     image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
-    image.write_tga_file("line1-1.tga");
+    image.write_tga_file("line1-2.tga");
     return 0;
 }

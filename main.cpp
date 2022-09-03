3678ad6e63835bdc09e62e70b8ec75fee8a7155e
zhangliang322 <lianzhang8099@163.com>
2022-09-03 13:43:40 +0800

    添加项目文件。

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include "tgaimage.h"
+const TGAColor white = TGAColor(255, 255, 255, 255);
+const TGAColor red = TGAColor(255, 0, 0, 255);
+//draw a line ,逐个顶点 0.01连成线
+void line(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color) {
+    for (float t = 0.; t < 1.; t += .01) {
+        int x = x0 + (x1 - x0) * t;
+        int y = y0 + (y1 - y0) * t;
+        image.set(x, y, color);
+    }
+}
+int main(int argc, char** argv) {
+    //文件绘制
+    TGAImage image(100, 100, TGAImage::RGB);
+    image.set(52, 41, red);
+    image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
+    image.write_tga_file("output.tga");
+    return 0;
+}

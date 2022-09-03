b116c89e50ae3dd577a5ced0b7915a2518b6f70c
zhangliang322 <lianzhang8099@163.com>
2022-09-03 15:04:41 +0800

    1

--- main.cpp
+++ main.cpp
@@ -74,6 +74,7 @@ int main(int argc, char** argv) {
     for (int i = 0; i < model->nfaces(); i++) {
         std::vector<int> face = model->face(i);
         for (int j = 0; j < 3; j++) {
+            //读取二维图像顶点
             Vec3f v0 = model->vert(face[j]);
             Vec3f v1 = model->vert(face[(j + 1) % 3]);
             int x0 = (v0.x + 1.) * width / 2.;
@@ -81,6 +82,7 @@ int main(int argc, char** argv) {
             int x1 = (v1.x + 1.) * width / 2.;
             int y1 = (v1.y + 1.) * height / 2.;
             line(x0, y0, x1, y1, image, white);
+            //只读取正视图v3不处理
         }
     }
 #pragma region 文件输出
